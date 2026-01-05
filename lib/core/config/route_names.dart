648709dbda66d3dfe118611ac918dac1a0b389e4
Ramadhan <whyramadhan27@gmail.com>
2026-01-05 09:11:51 +0700

    feat: adding API and Routes configuration, using ENV to hide ApiKey

---
+++ lib/core/config/route_names.dart
@@ -0,0 +1,6 @@
+class RouteNames {
+  static const String dashboard = '/';
+  static const String taskList = '/task-list';
+  static const String taskDetail = '/task-detail';
+  static const String addTask = '/add-task';
+}
